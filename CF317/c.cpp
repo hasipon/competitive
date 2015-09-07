bd1000a15af6d6343c7880f23d4e0b75b1b709d3
hasi <takuya.hashimoto@gmail.com>
2015-09-07 09:17:42 +0000

    CF317/c

--- CF317/c.cpp
+++ CF317/c.cpp
@@ -1,9 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
+vector<vector<pair<int,bool>>> var;
+vector<int> ans;
+vector<bool> sat;
+vector<vector<pair<pair<int,bool>,int>>> g;
+vector<bool> used;
+vector<int> color, vertices, cycle;
+bool found;
+int firstVertex;
+void dfs(int v) {
+	used[v] = true;
+	for (auto x : g[v]) {
+		int to = x.first.first;
+		if (!used[to]) {
+			ans[x.second] = x.first.second;
+			dfs(to);
+		}
+	}
+}
+bool findCycle(int v, int lastEdge) {
+	color[v] = 1;
+	vertices.push_back(v);
+	for (auto x : g[v]) {
+		if (x.second == lastEdge) {
+			continue;
+		}
+		int to = x.first.first;
+		if (color[to] == 1) {
+			ans[x.second] = x.first.second;
+			firstVertex = to;
+			found = true;
+			return true;
+		}
+		if (color[to] == 0) {
+			if (findCycle(to, x.second)) {
+				ans[x.second] = x.first.second;
+				cycle.push_back(v);
+				used[v] = true;
+				if (v == firstVertex) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+			if (found) {
+				return false;
+			}
+		}
+	}
+	color[v] = 2;
+	vertices.pop_back();
+	return false;
+}
 int main() {
+	ios_base::sync_with_stdio(false);
 	cin >> n >> m;
-	vector<vector<pair<int,bool>>> var(m);
+	var.resize(m);
 	for (int i = 0; i < n; ++ i) {
 		int k;
 		cin >> k;
@@ -17,8 +70,9 @@ int main() {
 			}
 		}
 	}
-	vector<int> ans(m);
-	vector<bool> sat(n);
+	ans.resize(m);
+	sat.resize(n);
+	g.resize(n);
 	for (int i = 0; i < m; ++ i) {
 		if (var[i].size() == 1) {
 			if (var[i][0].second) ans[i] = 1;
@@ -28,17 +82,30 @@ int main() {
 				if (var[i][0].second) ans[i] = 1;
 				sat[var[i][0].first] = sat[var[i][1].first] = true;
 			} else {
-				
+				g[var[i][0].first].push_back({var[i][1], i});
+				g[var[i][1].first].push_back({var[i][0], i});
 			}
 		}
 	}
-	bool yes = true;
-	for (bool x : sat) if (!x) { yes = false; break; }
-	if (yes) {
-		cout << "YES" << endl;
-		for (int x : ans) cout << x;
-		cout << endl;
-	} else {
-		cout << "NO" << endl;
+	used.resize(n);
+	for (int i = 0; i < n; ++ i) if (!used[i] && sat[i]) {
+		dfs(i);
 	}
+	color.resize(n);
+	for (int i = 0; i < n; ++i) if (!used[i]) {
+		vertices.clear();
+		found = false;
+		cycle.clear();
+		findCycle(i, -1);
+		if (!found) {
+			cout << "NO\n";
+			return 0;
+		}
+		for (int v : cycle) {
+			dfs(v);
+		}
+	}
+	cout << "YES" << endl;
+	for (int x : ans) cout << x;
+	cout << endl;
 }
